d36bfa5251cde496dd781891dae6de47a380c60f
Hlung <thongchaikol@gmail.com>
2015-03-20 18:41:42 +0700

    add demo project

--- iToastDemo/iToastDemo/ViewController.m
+++ iToastDemo/iToastDemo/ViewController.m
@@ -7,21 +7,44 @@
 //
 
 #import "ViewController.h"
+#import "iToast.h"
 
 @interface ViewController ()
-
+@property (strong, nonatomic) iToast *sharedToast;
+@property (assign, nonatomic) int queuedToastCount;
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (IBAction)showToast:(id)sender {
+    iToast *t = [iToast makeText:@"This is a TOAST! A simple way to show non blocking alert."];
+    [t setGravity:iToastGravityBottom];
+    [t setDuration:iToastDurationNormal]; // 3 sec
+    [t show];
+    self.sharedToast = t;
+}
+
+- (IBAction)hideToast:(id)sender {
+    [self.sharedToast dismiss];
+}
+
+- (IBAction)addToastToQueue:(id)sender {
+    NSString *s = [NSString stringWithFormat:@"This is a queued TOAST #%d !", ++self.queuedToastCount];
+    
+    iToast *t = [iToast makeText:s];
+    [t setGravity:iToastGravityBottom];
+    [t setDuration:iToastDurationShort]; // 1 sec
+    
+    [[iToastQueue shared] queueToast:t];
+}
+
+- (IBAction)cancelAllQueuedToasts:(id)sender {
+    
+    [[iToastQueue shared] cancelAllQueuedToasts];
 }
 
 @end
